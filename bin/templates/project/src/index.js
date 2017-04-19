0fe3548e477395665654fe6b468bafab3a42ae5d
Guangzhen Li <guangzhen.lgz@alibaba-inc.com>
2017-04-19 10:09:11 +0800

    BugID:10696456:Use H5Page for Domono mode
    Root Cause: New feature
    Solution: Use H5Page instead of Page
    Test Suggestion:cordova demo should be runned Ok
    
    Change-Id: Idf0229ed0477f965c59bcd5fb3fa2c70fc6a9e0e
    Reviewed-on: http://gerrit3.alibaba-inc.com:8080/403258
    Reviewed-by: 张婕欣 jessica.zjx <jessica.zjx@alibaba-inc.com>
    Reviewed-by: 贾香宏 xianghong.jxh <xianghong.jxh@alibaba-inc.com>
    Reviewed-by: 李广镇 guangzhen.lgz <guangzhen.lgz@alibaba-inc.com>

--- bin/templates/project/src/index.js
+++ bin/templates/project/src/index.js
@@ -19,68 +19,81 @@
  *
 */
 
-const Page = require('caf/page');
-const Path = require('path');
-const ConfigHelper = require('../CordovaLib/ConfigHelper');
 const pluginManager = require('../CordovaLib/PluginManager').getInstance();
+const ConfigHelper = require('../CordovaLib/ConfigHelper');
+const H5Page = require('yunos/page/H5Page');
 const Log = require('../CordovaLib/Log');
+const Path = require('path');
 
-Page.instance.on('create', function() {
-    function success(config) {
-        // Set config to PluginManager
-        pluginManager.config = config;
-        // Set page to PluginManager
-        pluginManager.page = Page.instance;
-        // Set log name and level
-        Log.setLogLevel(config.name, config.getPreferenceValue('LogLevel'));
-        // Add default yunos core plugin
-        pluginManager.addService('CoreYunOS', 'CordovaLib/CoreYunOSDomono', true);
-        // Initialize custom plugins
-        const PluginLoader = require('../CordovaLib/PluginLoader');
-        PluginLoader.init();
-        pluginManager.onCreate();
-        // Load the content path with Domono mode
-        let href = config.contentPath || 'index.html';
-        Page.window.href = Path.join('res', 'asset', href);
+class CordovaEmbedded extends H5Page {
+    onCreate() {
+        super.onCreate();
+        let self = this;
+        function success(config) {
+            // Set config to PluginManager
+            pluginManager.config = config;
+            // Set page to PluginManager
+            pluginManager.page = self;
+            // Set log name and level
+            Log.setLogLevel(config.name, config.getPreferenceValue('LogLevel'));
+            // Add default yunos core plugin
+            pluginManager.addService('CoreYunOS', 'CordovaLib/CoreYunOSDomono', true);
+            // Initialize custom plugins
+            const PluginLoader = require('../CordovaLib/PluginLoader');
+            PluginLoader.init();
+            pluginManager.onCreate();
+            // Load the content path with Domono mode
+            let href = config.contentPath || 'index.html';
+            self.url = Path.join('res', 'asset', href);
+        }
+        function error(msg) {
+            console.log('Failed to get content src:');
+            console.log(msg);
+        }
+        ConfigHelper.readConfig(success, error);
     }
-    function error(msg) {
-        console.log('Failed to get content src:');
-        console.log(msg);
+
+    // The event is fired when the page instance is started.
+    onStart() {
+        super.onStart();
+        pluginManager.onStart();
     }
-    ConfigHelper.readConfig(success, error);
-});
 
-// The event is fired when the page instance is started.
-Page.instance.on('start', function() {
-    pluginManager.onStart();
-});
+    // The event is fired when the page instance is stoped.
+    onStop() {
+        super.onStop();
+        pluginManager.onStop();
+    }
 
-// The event is fired when the page instance is stoped.
-Page.instance.on('stop', function() {
-    pluginManager.onStop();
-});
+    // The method is called when the page instance receives message from other pages.
+    onLink(link) {
+        super.onLink(link);
+        pluginManager.onLink(link);
+    }
 
-// The method is called when the page instance receives message from other pages.
-Page.instance.on('link', function(link) {
-    pluginManager.onLink(link);
-});
+    // The event is fired when the page instance is destroyed.
+    onDestroy() {
+        super.onDestroy();
+        pluginManager.onDestroy();
+    }
 
-// The event is fired when the page instance is destroyed.
-Page.instance.on('destroy', function() {
-    pluginManager.onDestroy();
-});
+    // The event is fired when the page instance is shown.
+    onShow() {
+        super.onShow();
+        pluginManager.onShow();
+    }
 
-// The event is fired when the page instance is shown.
-Page.window.on('show', function() {
-    pluginManager.onShow();
-});
+    // The event is fired when the page instance is hidden.
+    onHide() {
+        super.onHide();
+        pluginManager.onHide();
+    }
 
-// The event is fired when the page instance is hidden.
-Page.window.on('hide', function() {
-    pluginManager.onHide();
-});
+    // The event is fired when the page instance is requested to trim memory.
+    trimMemory() {
+        super.trimMemory();
+        pluginManager.onTrimMemory();
+    }
+}
 
-// The event is fired when the page instance is requested to trim memory.
-Page.window.on('trimMemory', function() {
-    pluginManager.onTrimMemory();
-});
+module.exports = CordovaEmbedded;
